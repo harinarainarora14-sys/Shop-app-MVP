e5fc6e66fd3a7f4d1e16ae7bf9b68d041397652b
harinarainarora14-sys <harinarainarora14@gmail.com>
2025-09-23 19:27:06 +0530

    Update dialog.tsx

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -94,7 +94,7 @@ function DialogFooter({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="dialog-footer"
-      className={cn(
+      className={
         'flex flex-col-reverse gap-2 sm:flex-row sm:justify-end',
         className,
       )}
